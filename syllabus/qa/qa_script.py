02f3d5d0a5d0e5d30fb015be5ceee7f08589cea4
Amer Jusupovic <amerjusupovic00@gmail.com>
2019-11-11 18:15:49 -0600

    edit qa_script and remove_noise to help with speed and easily recognize obvious answers

--- syllabus/qa/qa_script.py
+++ syllabus/qa/qa_script.py
@@ -8,7 +8,7 @@ from remove_noise import clean
 def main():
     # Loading data and filtering / preprocessing the documents
     df = pdf_converter(directory_path='/mnt/c/Users/amerj/Documents/python_atom/nlp_syllabus/syllabus_run_files')
-    print(df.dtypes)
+    # print(df.dtypes)
     df = clean(df)
 
     # Download if model not already downloaded
@@ -22,10 +22,11 @@ def main():
     cdqa_pipeline.fit_retriever(df)
     #cdqa_pipeline.fit_reader("/mnt/c/Users/amerj/Documents/python_atom/nlp_syllabus/trained_sets/10_26_2019.json")
 
-    return cdqa_pipeline
+    print(df)
+    return cdqa_pipeline, df
 
 # store keys with colons following string, data in next string is value? then default to predict?
-def run_queries(cdqa_pipeline):
+def run_queries(cdqa_pipeline, df):
     qa_txt = """Who is the professor?^Who is the TA?^What is the grading policy?^What is the grade distribution?^What materials are required?
     ^What is the professor’s email?^What is the textbook?^What is the homework for October 13th?^Where is class?^Where are lectures?^Where are discussions?
     ^Where is homework posted?^Where are the professor’s office hours?^Where are the TA’s office hours?^When are tests?^When are quizzes?
@@ -38,16 +39,32 @@ def run_queries(cdqa_pipeline):
     # Sending a question to the pipeline and getting prediction
     i = 1
     for query in queries:
-        prediction = cdqa_pipeline.predict(query)
+        found = False
+        for section in df['paragraphs']:
+            for paragraph in section:
+                for word in query.lower().split():
+                    if word[-1:] == "?":
+                        wordCheck = word[:-1].lower() + ":"
+                    else:
+                        wordCheck = word.lower() + ":"
+                    if wordCheck in paragraph.lower().split():
+                        print("Query Number: " + str(i))
+                        print('Query: {}'.format(query))
+                        print('Answer: {}\n'.format(paragraph))
+                        found = True
+                        break
 
-        print("Query Number: " + str(i))
-        print('Query: {}'.format(query))
-        print('Answer: {}'.format(prediction[0]))
-        print('Title: {}'.format(prediction[1]))
-        print('Paragraph: {}\n'.format(prediction[2]))
+        if not found:
+            prediction = cdqa_pipeline.predict(query)
+
+            print("Query Number: " + str(i))
+            print('Query: {}'.format(query))
+            print('Answer: {}'.format(prediction[0]))
+            print('Title: {}'.format(prediction[1]))
+            print('Paragraph: {}\n'.format(prediction[2]))
 
         i+=1
 
 if __name__ == '__main__':
-    cdqa_pipeline = main()
-    run_queries(cdqa_pipeline)
+    cdqa_pipeline, df = main()
+    run_queries(cdqa_pipeline, df)
