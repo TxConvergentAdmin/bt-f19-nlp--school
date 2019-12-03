f9cad5c9576c05e73f7cfc386a0b4228772cc3c0
Shrivu Shankar <shrivu1122@gmail.com>
2019-12-02 18:17:54 -0600

    ✏️: Added New Frontend

--- syllabus/app/src/App.js
+++ syllabus/app/src/App.js
@@ -1,26 +1,138 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Accordion, Card, Button } from 'react-bootstrap';
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //navigationBar at the top
+    <React.Fragment>
+      <Navbar bg="light" expand="lg" className = "menu">
+        <Navbar.Brand href="#home" className="mainTitle"> MyUTSyllabus</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            {/* <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link> */}
+            <NavDropdown title="Doe. J" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Change Account</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Different Class</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4"> Your Schedule </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <h1 className = "titleOfClass"> Math 408M: Nicolas Reyes</h1>
+      <div className = "flex-acc-pdf">
+        <div className = "flex-acc">
+          <Accordion className = "accordian">
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                  Summary Information
+            </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>ENTER RELEVANT INFORMATION</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                  Contact Info/Office Hours
+            </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>ENTER RELEVANT INFORMATIONy</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                  Ratings
+            </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="2">
+                <Card.Body>ENTER RELEVANT INFORMATION</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="3">
+                  Tests and Quizzes
+            </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="3">
+                <Card.Body>ENTER RELEVANT INFORMATION</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="4">
+                  Homework and Assignments
+            </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="4">
+                <Card.Body>ENTER RELEVANT INFORMATION</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="5">
+                  Grading Policy and Distribution
+            </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="5">
+                <Card.Body>ENTER RELEVANT INFORMATION</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="6">
+                  Required Materials
+            </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="6">
+                <Card.Body>ENTER RELEVANT INFORMATION</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+          </div>
+      </div>
+    </React.Fragment>
   );
+  // I think this should be it for rendering the syllabus, once you enter the directory name. LET ME KNOW if it doesn't work
+  // After yout get it to work, you shoudl just be able to enter the flexbox specifications under App.CSS. 
+  // ReactPDF.render(<MyDocument />, `${__dirname}/example.pdf`);
 }
 
 export default App;
